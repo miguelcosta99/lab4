02724c93c41e448c2d3c9488c80474fad16cbabb
Ubuntu <ubuntu@ip-172-31-62-192.ec2.internal>
2020-10-26 01:16:59 +0000

    lab4

--- lab4.py
+++ lab4.py
@@ -20,3 +20,17 @@ timer = rospy.Timer(rospy.Duration(1), timerCallBack)
 sub = rospy.Subscriber('/sum', String, matriculaCallBack)
 
 rospy.spin()
+
+#- Criar um código no Development Environment do Robomaker;
+
+#- Criar um repositório no Github com o email da UNIFEI do aluno;
+
+#- Criar um nó que envia o seu número de matrícula por um tópico;
+
+#- Criar um nó que recebe o número de matrícula pelo tópico mencionado acima e retorna por um tópico para o primeiro nó a soma dos números;
+
+#- A soma deve ser printada na tela durante a calback do tópico, no primeiro nó;
+
+#- Utilizar os comandos de terminal (git add, git commit, git push, etc.) para enviar o código para o Github (link nos comentários da tarefa);
+
+#- Disponibilizar o link do github do aluno com o código.
